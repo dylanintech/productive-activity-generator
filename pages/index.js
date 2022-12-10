a6df0174754ca697c906c7a440325e1eb9a31ef5
Dylan Molina <dylanivanmolina@gmail.com>
2022-12-10 16:09:16 -0600

    edited title

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Productive Activity Generator | by dylan</title>
       </Head>
       <div className="container">
         <div className="header">
