d07b6b9048cd9a89286990dbe61738a15106b8ca
Dylan Molina <dylanivanmolina@gmail.com>
2023-01-07 21:29:32 -0600

    edited output header

--- pages/index.js
+++ pages/index.js
@@ -119,7 +119,7 @@ const Home = () => {
             <div className='output'>
               <div className='output-header-container'>
                 <div className='output-header'>
-                  <h3>Output</h3>
+                  <h3>your activities 📈</h3>
                 </div>
               </div>
               <div className='output-content'>
